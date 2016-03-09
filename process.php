93efbabbc32e24b86474e54cbfc29407320a9af0
Thomas <thomas.maurier91@gmail.com>
2016-03-09 17:02:43 +0100

    chart

--- process.php
+++ process.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'common.php';
 
 try {
     $db = new PDO('mysql:host=localhost;dbname=BaseLink', "root");
@@ -104,22 +105,33 @@ for ( $i=1;$i<=4;$i++){
 <!DOCTYPE HTML>
 <html>
 <head>
+    <meta charset="utf-8">
+
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import styles-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script src="http://code.highcharts.com/highcharts.js"></script>
     <script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
 </head>
+
 <script>
     $(document).ready(function() {
         var chart1 = new Highcharts.Chart({
 
             chart: {
                 spacingBottom: 15,
-                spacingTop: 10,
-                spacingLeft: 10,
-                spacingRight: 10,
-                width: 1000,
-                height: null,
+                spacingTop: 100,
+                spacingLeft: 200,
+                spacingRight: 200,
+                width: null ,
+                height: 500,
                 renderTo: 'container',
-                type: 'column'
+                type: 'bar'
             },
 
             title: {
@@ -160,12 +172,50 @@ for ( $i=1;$i<=4;$i++){
 
 
 </script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".dropdown-button").dropdown();
+        document.getElementById("label1").style.visibility='hidden';
 
+    });
+</script>
 <body>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+
+<header>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper">
+                <img src="images/sfr_business_logo_white_nobsl-1.png" class="brand-logo" alt="SFR Business"/>
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a href="index.php?lang=fr"><img src="images/FlagFR.png" height="20px" width="20px"> Francais</a></li>
+                    <li><a href="index.php?lang=en"><img src="images/FlagEN.png" height="20px" width="20px"> English</a></li>
+
+                </ul>
+                <ul class="right hide-on-med-and-down"><li>
+                        <a class="dropdown-button" href="#!" data-activates="dropdown1"><?php echo $lang['LANGUAGE'];?><i class="material-icons right">arrow_drop_down</i></a>
+                    </li></ul>
+            </div>
+        </nav>
+    </div>
+</header>
+<main>
 
 <div id="container" style="width:100%; height:400px;">
 
 
 </div>
+<!--================== FOOTER =============================-->
+</main>
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <img src="images/sfr_business_logo_white_nobsl-1.png" alt="SFR BUSINESS">
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
